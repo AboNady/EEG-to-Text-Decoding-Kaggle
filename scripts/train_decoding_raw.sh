ccb546dbdc054224140b176a524ca6a7730a8265
Mahmoud Nady <111538752+AboNady@users.noreply.github.com>
2024-11-04 21:48:07 +0200

    2 stepes

--- scripts/train_decoding_raw.sh
+++ scripts/train_decoding_raw.sh
@@ -1,6 +1,6 @@
 python /kaggle/working/EEG-to-Text-Decoding/train_decoding_raw.py --model_name BrainTranslator \
     --task_name task1_task2_taskNRv2 \
-    --one_step \
+    --two_step \
     --pretrained \
     --not_load_step1_checkpoint \
     --num_epoch_step1 1 \
