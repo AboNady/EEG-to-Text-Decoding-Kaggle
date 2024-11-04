6062bfd072e33900a93dd2d856fa2c8d26e6e8b4
Mahmoud Nady <111538752+AboNady@users.noreply.github.com>
2024-11-04 21:33:40 +0200

    added a comment

--- scripts/train_decoding_raw.sh
+++ scripts/train_decoding_raw.sh
@@ -10,3 +10,22 @@ python /kaggle/working/EEG-to-Text-Decoding/train_decoding_raw.py --model_name B
     -b 32\
     -s /kaggle/working/checkpoints/decoding_raw \
     -cuda cuda:0
+
+
+'''
+python /kaggle/working/EEG-to-Text-Decoding/train_decoding_raw.py --model_name BrainTranslator \
+    --task_name task1_task2_taskNRv2 \
+    --one_step \
+    --pretrained \
+    --not_load_step1_checkpoint \
+    --num_epoch_step1 1 \
+    --num_epoch_step2 1 \
+    -lr1 0.5 \
+    -lr2 0.5 \
+    -b 32\
+    -s /kaggle/working/checkpoints/decoding_raw \
+    -cuda cuda:0
+
+
+
+'''
