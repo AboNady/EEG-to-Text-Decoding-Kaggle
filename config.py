f31a40f4babe79199ac6f9a0c256d654683a5f8e
Mahmoud Nady <111538752+AboNady@users.noreply.github.com>
2024-11-04 22:08:27 +0200

    Update config.py

--- config.py
+++ config.py
@@ -18,9 +18,9 @@ def get_config(case):
         parser.add_argument('-m', '--model_name', help='choose from {BrainTranslator, BrainTranslatorNaive}', default = "BrainTranslator" ,required=True)
         parser.add_argument('-t', '--task_name', help='choose from {task1,task1_task2, task1_task2_task3,task1_task2_taskNRv2}', default = "task1", required=True)
         
-        parser.add_argument('-1step', '--one_step', dest='skip_step_one', action='store_false')
-        parser.add_argument('-2step', '--two_step', dest='skip_step_one', action='store_true')
-
+        parser.add_argument('--two_step', dest='two_step', action='store_true', help='Run the two-step training processn when true')
+        parser.add_argument('--one_step', dest='two_step', action='store_false', help='Run only the first step training process when false')
+        
 #        when we want to reun step 2 directly with no load in step 1
 #        parser.add_argument('-1step', '--one_step', dest='skip_step_one', action='store_true')
 #        parser.add_argument('-2step', '--two_step', dest='skip_step_one', action='store_false')
