eaa7325c5824c69e5982e66fe279854064d7c4fb
Mahmoud Nady <111538752+AboNady@users.noreply.github.com>
2024-11-04 22:37:14 +0200

    Update train_decoding_raw.sh

--- scripts/train_decoding_raw.sh
+++ scripts/train_decoding_raw.sh
@@ -2,7 +2,7 @@ python /kaggle/working/EEG-to-Text-Decoding/train_decoding_raw.py --model_name B
     --task_name task1_task2_taskNRv2 \
     --two_step \
     --pretrained \
-    --not_load_step1_checkpoint \
+    #--not_load_step1_checkpoint \
     --num_epoch_step1 1 \
     --num_epoch_step2 1 \
     -lr1 2 \
